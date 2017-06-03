0da3f048c1dfac34fb76f2ad9b15ecd3b552ccd8
mtrieu <mtrieu@mfour.com>
2017-06-03 15:37:20 -0700

    Added HeaderToolbar component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <header-toolbar></header-toolbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import HeaderToolbar from '@/components/HeaderToolbar';
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HeaderToolbar
+  }
 }
 </script>
 
 <style>
+body {
+  margin:0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
