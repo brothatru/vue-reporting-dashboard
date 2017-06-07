92ee8444af3243ad6ee25bd0bf735af435831c80
mtrieu <mtrieu@mfour.com>
2017-06-07 00:03:58 -0700

    Added first list item

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="sidenav">
-
+    <ul class="sidenav-ul">
+      <a v-for="item in items" href="javascript:;">
+        <!-- <li>{{item.title}}</li> -->
+        <li>{{item.title}}</li>
+      </a>
+    </ul>
   </div>
 </template>
 
@@ -9,6 +14,13 @@ export default {
   name: 'SideNav',
   components: {
 
+  },
+  data() {
+    return {
+      items: [
+        { title: "Menu Item 1", link: "#" }
+      ]
+    }
   },
   mounted() {
     console.log('sidenav loaded');
@@ -22,6 +34,8 @@ export default {
 * Get some ideas from this bootstrap material design website
 * http://code.band-x.media/SASS-Material-Design-for-Bootstrap/layout/navigation-drawer/
 */
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,400i,700');
+
 .sidenav {
   position:fixed;
   top:52px;
@@ -29,5 +43,23 @@ export default {
   background-color:#333;
   width:300px;
   height:100%;
+  color:#fff;
+  font-family: 'Roboto', sans-serif;
+  letter-spacing: 0.03em;
+}
+.sidenav-ul {
+  border:1px red dashed;
+  width:100%;
+  height:100%;
+  list-style:none;
+  padding:0
+}
+.sidenav-ul a {
+  display: block;
+  color:#fff;
+  font-size:16px;
+}
+hr.divider {
+
 }
 </style>
